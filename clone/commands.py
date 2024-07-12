8ae16059fba685123aa251c0d9c1f4b28cfe80a1
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-07-12 07:59:54 +0530

    Update commands.py

--- clone/commands.py
+++ clone/commands.py
@@ -157,8 +157,7 @@ async def start(client, message):
             )
             filetype = msg.media
             file = getattr(msg, filetype.value)
-            title = '@TheBlackXYZ  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
-            size=get_size(file.file_size)
+            title = '@TheBlackXYZ  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files1.file_name.split()))
             f_caption = f"<code>{title}</code>"
             await msg.edit_caption(
                 caption=f_caption,
@@ -173,8 +172,7 @@ async def start(client, message):
             pass
         return await message.reply('No such file exist.')
     files = files_[0]
-    title = '@TheBlackXYZ  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
-    size=get_size(files.file_size)
+    title = '@TheBlackXYZ  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files1.file_name.split()))
     f_caption=files.caption
     if f_caption is None:
         f_caption = f"@TheBlackXYZ  {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))}"
